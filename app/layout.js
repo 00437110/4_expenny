1373423ae6896da158ada185d5cb2fd3b68588b3
Erick Domínguez <50849788+00437110@users.noreply.github.com>
2025-08-09 02:53:15 -0600

    Chapter 4.2 completed login inputs done

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@
 import "./globals.css";
 import "./fanta.css"
 import Head from "./Head";
+import Link from "next/link";
+import GoTo from "@/components/GoTo";
 
 export const metadata = {
   title: "Expenny ⋅ The Subscription Tracker",
@@ -9,11 +11,52 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const header = (
+    <header>
+      <div>
+        <Link href={'/'}>
+          <h1 className="text-gradient">Expenny</h1>
+        </Link>
+        <p>The Subscription Tracker</p>
+      </div>
+      <GoTo></GoTo>
+    </header>
+  )
+  const footer = (
+    <footer>
+      <div className="hard-line" />
+      <div className="footer-content">
+        <div>
+          <div>
+
+            <h4>Expenny</h4>
+            <p>|</p>
+            <button disabled>Install App</button>
+          </div>
+          <p className="copyright">© Copyright 2024-2025, James McArthur.<br />All rights reserved.</p>
+        </div>
+        <div>
+          <p>Facing issues? <a>Get help</a></p>
+          <p>Suggestions for improvement? <a>Share feedback</a></p>
+          <div>
+            <Link href={'/privacy'}>Privacy Policy</Link>
+            <Link href={'/tos'}>Terms of Service</Link>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  )
   return (
     <html lang="en">
-      <Head/>
+      <Head />
       <body >
-        {children}
+        {header}
+        <div className="full-line" />
+        <main>
+          {children}
+        </main>
+        {footer}
       </body>
     </html>
   );
