10c1b781ae3aeb0bad67b0ffb7903c49e0a0a3ba
Erick Domínguez <50849788+00437110@users.noreply.github.com>
2025-08-12 20:03:36 -0600

    Chapter 4.6 complete, the styling of the dashboard

--- components/SubscriptionsDisplay.jsx
+++ components/SubscriptionsDisplay.jsx
@@ -22,44 +22,40 @@ export default function SubscriptionsDisplay() {
                                     ' card-button-primary' : ' card-button-secondary')}>
                                     <small>{status}</small>
                                 </div>
+                            </div>
+                            <p><i>{category}</i></p>
 
-                                <p><i>{category}</i></p>
-
-                                <div className="sub-cost">
-                                    <h2>$ {cost}</h2>
-                                    <p>{currency}</p>
-                                </div>
-
-                                <small>per {billingFrequency}</small>
-
-                                <div className="sub-renewal">
-                                    <div>
-                                        <p>Started</p>
-                                        <h4>{startDate}</h4>
-                                    </div>
-                                    <div>
-                                        <p>Due</p>
-                                        <h4>{getDaysUntilNextCharge(startDate, billingFrequency)}</h4>
-                                    </div>
-                                    <div className="white-line" />
-                                    <p>{notes}</p>
-
-                                    <div className="subscription-actions">
-                                        <button className="button-card">
-                                            <i class="fa-solid fa-pen-to-square"></i>
-                                            Edit
-                                        </button>
-                                        <button className="button-card">
-                                            <i className="fa-solid fa-trash"></i>
-                                            Delete
-                                        </button>
-
-                                        </div>
-                                </div>
-
+                            <div className="sub-cost">
+                                <h2>$ {cost}</h2>
+                                <p>{currency}</p>
                             </div>
 
+                            <small>per {billingFrequency}</small>
 
+                            <div className="sub-renewal">
+                                <div>
+                                    <p>Started</p>
+                                    <h4>{startDate}</h4>
+                                </div>
+                                <div>
+                                    <p>Due</p>
+                                    <h4>{getDaysUntilNextCharge(startDate, billingFrequency)}</h4>
+                                </div>
+                            </div>
+                            <div className="white-line" />
+                            <p>{notes}</p>
+
+                            <div className="subscription-actions">
+                                <button className="button-card">
+                                    <i className="fa-solid fa-pen-to-square"></i>
+                                    Edit
+                                </button>
+                                <button className="button-card">
+                                    <i className="fa-solid fa-trash"></i>
+                                    Delete
+                                </button>
+
+                            </div>
                         </div>
                     )
                 })}
